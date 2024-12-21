a240a1e5e5bcb5dae22f7f46fda516509b6bf8a2
Asal Samarasinghe <104274406+Asal30@users.noreply.github.com>
2024-12-22 02:28:37 +0530

    Updated user login function with PW hashing & refactoring code as simplify

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,49 +1,56 @@
 import User from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import argon from 'argon2'
 
-export function registerUser(req,res){
+export async function registerUser(req, res) {
+    try {
+        const user = req.body;
+        user.password = await argon.hash(user.password);
 
-    const user = req.body
+        const newUser = new User(user);
+        await newUser.save();
 
-    const newUser = new User(user)
-    newUser.save().then(
-        ()=>{
-            res.json({
-                message : "User registered successfully"
-            })
-        }
-    ).catch(
-        ()=>{
-            res.status(500).json({
-                message : "Error creating user account"
-            })
-        }
-    )
-
+        res.json(
+            { message: "User registered successfully" }
+        );
+    } catch (err) {
+        res.status(500).json(
+            { message: "Error creating user account", error: err.message }
+        );
+    }
 }
-export function loginUser(req,res){
-    const credentials = req.body
-    User.findOne({email : credentials.email, password : credentials.password}).then(
-        (response)=>{
-            if(response == null){
-                res.status(404).json({
-                    message : "Invalid email or password"
-                })
-            }else{
-                const payload = {
-                    id : response._id,
-                    email : response.email,
-                    password : response.password,
-                    type : response.type,
-                    firstName : response.firstName,
-                    lastName : response.lastName,
-                };
-                const token = jwt.sign(payload, "secretKey");
-                res.json({
-                    message : "User logged in successfully",
-                    token : token
-                })
-            }
+
+export async function loginUser(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json(
+                { message: "Invalid email or password" }
+            );
         }
-    )
+
+        const isPasswordValid = await argon.verify(user.password, password);
+        if (!isPasswordValid) {
+            return res.status(400).json(
+                { message: "Invalid email or password" }
+            );
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                email: user.email,
+                type: user.type,
+                firstName: user.firstName,
+                lastName: user.lastName,
+            },
+            "secretKey"
+        );
+
+        res.json({ message: "User logged in successfully", token });
+    } catch (err) {
+        res.status(500).json({ message: "Server error", error: err.message });
+    }
 }
