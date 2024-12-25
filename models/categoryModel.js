f324c83d5ba99387bb8a7f56739bd10b7ad79f3d
Asal Samarasinghe <asalsamarasinghe30@gmail.com>
2024-12-25 14:05:42 +0530

    Implementing categoryItemModel

---
+++ models/categoryModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const categoryItemModel = mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true,
+            unique : true
+        },
+        image : {
+            type : String,
+            required : true
+        },
+        description : {
+            type : String,
+            required : true
+        },
+        price : {
+            type : Number,
+            required : true
+        }
+    }
+)
+
+const CategoryItem = mongoose.model("categoryItem", categoryItemModel)
+
+export default CategoryItem;
