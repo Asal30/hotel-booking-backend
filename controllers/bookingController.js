a319bddf1f007dbce46371f6524f90697a551402
Asal Samarasinghe <asalsamarasinghe30@gmail.com>
2024-12-29 20:41:54 +0530

    Refactor create booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,41 +15,24 @@ export async function getBookings(req, res) {
 }
 
 export async function createBooking(req, res) {
-    const {roomId, email, startDate, endDate} = req.body;
+    const { roomId, email, startDate, endDate } = req.body;
+
     if (!roomId || !email || !startDate || !endDate) {
-        return res.status(400).json(
-            { message: "Missing required fields" }
-        );
+        return res.status(400).json({ message: "Missing required fields" });
     }
+
     if (new Date(startDate) >= new Date(endDate)) {
-        return res.status(400).json(
-            { message: "Invalid dates!!! Start date must be earlier than end date" }
-        );
+        return res.status(400).json({ message: "Invalid dates! Start date must be earlier than end date" });
     }
+
     try {
-        const bookings = await Booking.countDocuments({});
-        
-        const newBooking = new Booking({
-            bookingId: bookings + 1,
-            roomId,
-            email,
-            startDate,
-            endDate
-        });
-        
-        await newBooking.save();
-        
+        const bookingId = (await Booking.countDocuments()) + 1;
+        const newBooking = await Booking.create({ bookingId, roomId, email, startDate, endDate });
+
         res.json(
-            {
-                message: "Booking saved to database successfully",
-                data: newBooking
-            }
+            { message: "Booking saved to database successfully", data: newBooking }
         );
-        
     } catch (err) {
-        res.status(500).json({
-            message: "Error finding booking list",
-            error: err.message,
-        });
+        res.status(500).json({ message: "Error creating booking", error: err.message });
     }
 }
